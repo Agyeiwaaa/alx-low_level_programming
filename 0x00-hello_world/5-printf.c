2f580228604705924f53c0af5e29d25bc2bdad21
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-20 15:24:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,13 @@
 /**
  * main - C program that prints a message with prinf function
  * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");		
 return (0);
 }
