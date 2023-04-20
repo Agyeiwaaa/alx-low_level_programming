89a9ae09e34beda997328396a12e3633e2536e57
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-20 10:49:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* Programming is like building a multilingual puzzle */
+/* Return: 0 (success) */
+int main (void)
+  {
+	  puts( "\"Programming is like building a multilingual puzzle");
+	  return (0);
+   }
