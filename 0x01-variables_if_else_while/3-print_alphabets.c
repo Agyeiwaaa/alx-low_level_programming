dd57207cb7c83df7faab8d2b5effd11147f9ec1d
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-22 08:11:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: A program that prints both lowercase and uppercase alphabets followed by new line
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int b;
+
+for (b = 0; b < 52; b++)
+{
+putchar(alph[b]);
+}
+putchar('\n');
+return (0);
+}
