fe9a272018b892296dd4fd7a7fd3edcf18cd4550
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-24 15:36:24 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - C program that prints numbers from 0 to 9.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int x;
+
+for (x = 36;  < 46; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
