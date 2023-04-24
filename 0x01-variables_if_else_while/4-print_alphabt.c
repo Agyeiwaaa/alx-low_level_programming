2ef1753fd38a5b820fc8384ca567aaf2e9e8243f
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-24 14:54:13 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+* main - C program that prints alphabets in both upper and lower cases except q and e
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int x;
+
+for (x = 65; x < ; x++)
+{
+if (x != 65 && x != 90)
+{
+putchar(x);
+}
+}
+putchar('\n');
+return (0);
+}
