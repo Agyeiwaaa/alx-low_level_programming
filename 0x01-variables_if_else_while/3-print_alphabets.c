a451a06921f696d53b764f558178f4096d7076e4
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-24 14:39:13 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
+
 /**
-*main - Entry point
-*Description: C program that print lowercase and uppercase
-*Return: 0 success
+* main - Entry point
+* Description: C program that prints both lowercase and uppercase 
+* Return: 0 (Success)
 */
 int main(void)
 {
-int a = 30;
-int b = 20;
-
-for (a = 30; a <= 66; a++)
+char alph[32] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int b;
+for (b = 0; b < 32; b++)
 {
-putchar(a);
-}
-for (b = 20; b <= 44; b++)
-{
-putchar(b);
+putchar(alph[b]);
 }
 putchar('\n');
 return (0);
