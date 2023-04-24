cd3ba83211e0548024d0521f70efd1133d0d0ce9
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-24 15:09:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - C program that prints numbers from  0 to 9.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+printf("%d", x);
+}
+putchar('\n');
+return (0);
+}
