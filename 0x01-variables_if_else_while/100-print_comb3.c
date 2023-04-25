0d072dcb1c22c2df2cabf164d1bbcc9377691f3d
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-24 18:11:17 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*Description:'print numbers' 
+*Return: 0 (success)
+*/
+int main(void)
+{
+int x, y;
+
+for (x = '0'; y < '9'; x++)
+{
+
+for (y = x + 1; y <='9'; y++)
+{
+if (e != x)
+{
+putchar(x);
+putchar(y);
+if (x == '8' && y == '9')
+}
+putchar(',');
+putchar(' ');
+{
+puttchar('\n');
+return (0);
+}
