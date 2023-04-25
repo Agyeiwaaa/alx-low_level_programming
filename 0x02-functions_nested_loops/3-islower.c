da18493ad9e288d5cd5e33458fef49581e4db9a9
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-25 14:25:36 -0700

    functions and nested loop task 0-5

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,13 @@
-#include "main.h"
-
+include "main.h"
 /**
-* main - Entry point
-* Description:check if char is lowercase @c: is the char to be checked
-* Return: 1 if char is lowercase, otherwise 0.
-*/
-
+*  * _ - check if char is lowercase
+*   * @c: is the char to be checked
+*    * Return: 1 if char is lowercase, otherwise 0.
+*     */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
-return (1);
-else
+return (1);		        
+else	
 return (0);
 }
-
