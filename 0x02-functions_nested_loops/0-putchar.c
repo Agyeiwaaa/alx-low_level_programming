25a32d537e02c3bbe682938ce52ede4a504da579
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-25 13:48:39 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
 * main - Entry point
 * Description: A program that prints _putchar 
-* Return: always 0 (success)
+* Return: 0 (success)
 */
 int main(void)
 {
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* Description - print all alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
+}
+
