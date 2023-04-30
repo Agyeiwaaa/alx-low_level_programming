ec7116122efbfb32bdbfa2ae7c565c23e29e7736
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-30 16:01:52 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,19 +11,16 @@ _putchar('\n');
 } else
 {
 int i, j;
-
 for (i =1; i <= size; i++)
 {
 for (j = i; j < size; j++)
 {
 _putchar(' ');
 }
-
 for (j = 1; j <= i; j++)
 {
 _putchar('#');
 }
-
 _putchar('\n');
 }
 }
