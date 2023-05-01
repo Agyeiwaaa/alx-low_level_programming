226eafd31db289de342cdad66b745d75ea2ce160
Agyeiwaaa <theresatawiah686@gmail.com>
2023-04-30 17:40:43 -0700

    0x02-functions_nested_loops task 6-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - adds two integers and returns the result
+* @x: int to be added to y
+* @y: int to be added to x
+* Return: sum of x and y
+*/
+int add(int x, int y)
+{
+int z;
+z = a + b;
+return (z);
+}
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,11 @@
-include "main.h"
-/**
-*  * _ - check if char is lowercase
-*   * @c: is the char to be checked
-*    * Return: 1 if char is lowercase, otherwise 0.
-*     */
-int _islower(int c)
+slower - check if char is lowercase
+ * @c: is the char to be checked
+ *  * Return: 1 if char is lowercase, otherwi:wq
+	*  */
+ int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-return (1);		        
-else	
-return (0);
+	        if (c >= 'a' && c <= 'z')
+			                return (1);
+		        else
+				                return (0);
 }
