46ba9cbb4accb30049cb22030dae76a058ff1268
Agyeiwaaa <theresatawiah686@gmail.com>
2023-05-01 14:03:14 -0700

    task 5 and 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
+
 #include "main.h"
 /**
 * add - adds two integers and returns the result
-* @x: int to be added to y
-* @y: int to be added to x
-* Return: sum of x and y
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: sum of a and b
 */
-int add(int x, int y)
-{
-int z;
-z = a + b;
-return (z);
+int add(int a, int b)
+int c;
+c = a + b;
+return (c);
 }
