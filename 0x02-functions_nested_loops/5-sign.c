e046a71b4b4c013c756a324fa13272e3ad41e283
Agyeiwaaa <theresatawiah686@gmail.com>
2023-05-02 17:01:58 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,12 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-{
 return (1);
+} else if (n == 0)
 }
-else if (n == 0)
-}  
-_putchar(48);
-{
+putchar(48);
 return (0);
-} 
-else if (n < 0)
+} else if (n < 0)
 {
 _putchar('-');
 }
